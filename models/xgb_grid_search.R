a18b3fb2c66e10c2566a1e16642618a8b13ff8c8
Eric Kramer <ericransomkramer@gmail.com>
2015-05-01 13:22:41 +0200

    train and predict finished

--- models/xgb_grid_search.R
+++ models/xgb_grid_search.R
@@ -4,7 +4,7 @@ library("BatchJobs")
 
 ## train and predict function 
 
-train_and_predict = function(params, ...){
+train_and_predict = function(params, at, ...){
   
   require("dplyr")
   require("Matrix")
@@ -25,18 +25,31 @@ train_and_predict = function(params, ...){
     y - 1
   }
   
-
+  predict_matrix = function(x, m){
+    p = lapply(at, function(ntreelimit){
+      predict(m, x, ntreelimit=ntreelimit) %>%
+        matrix(ncol=9, byrow=T)
+    })
+  }
   
+  load("./data/sets.Rdata")
+  
+  x = lapply(sets, create_matrix)
+  y = lapply(sets, create_target)
+  
+  m = xgboost(param=params,
+              data=x[[1]],
+              label=y[[1]],
+              ...)
+  
+  
+  p = lapply(x, predict_matrix, m)
+  return(p)
 }
 
-x = train1 %>%
-  select(starts_with("feat")) %>%
-  as.matrix %>%
-  apply(2, as.numeric) %>%
-  Matrix
 
-y = train1$target %>%
-  factor %>%
-  as.numeric 
-
-y = y-1
+train_and_predict(params, 
+                  metrics=list("mlogloss"),
+                  objective="multi:softprob",
+                  nrounds=10,
+                  num_class=9)
