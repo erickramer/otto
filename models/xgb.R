85e1003f8026936338c13ceebe740063be91ebfe
Eric Kramer <ericransomkramer@gmail.com>
2015-05-01 13:23:03 +0200

    messing around with xgb

--- models/xgb.R
+++ models/xgb.R
@@ -1,17 +1,19 @@
 library("xgboost")
 library("Matrix")
 
-x = train %>%
+load("./data/train1")
+
+x = train1 %>%
   select(starts_with("feat")) %>%
   as.matrix %>%
   apply(2, as.numeric) %>%
   Matrix
 
-y = train$target %>%
+y = train1$target %>%
   factor %>%
   as.numeric 
 
-y = y -1
+y = y-1
 
 params = list(
   objective="multi::softprob",
@@ -20,10 +22,11 @@ params = list(
   )
 
 m = xgboost(params=params,
-            data=x, 
-            nrounds=30,
-            num_class=9,
-            nfold=3,
-            label=y,,
-            metrics=list("mlogloss"),
-            objective="multi:softprob")
+          data=x, 
+          nrounds=10,
+          num_class=9,
+          label=y[[1]],
+          metrics=list("mlogloss"),
+          objective="multi:softprob")
+
+
