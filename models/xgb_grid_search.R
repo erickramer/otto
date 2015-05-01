3a13f724bbe3e93a5b956e157799b409579ecfb4
Eric Kramer <ericransomkramer@gmail.com>
2015-05-01 16:23:53 +0200

    get rid of packrat stuff

--- models/xgb_grid_search.R
+++ models/xgb_grid_search.R
@@ -1,4 +1,14 @@
-packrat::init("~/Projects/otto")
+### TRAINING PARAMETERS ###
+
+objective = "multi:softprob"
+metrics=list("mlogloss")
+
+at = 2^(4:12) # number of trees to use
+eta=c(0.03, 0.1, 0.3, 1) # training step
+max.depth=1:12 # depth of tree
+subsample=c(0.5, 0.75, 1) # subsampling for tree training
+
+### SCRIPT ####
 
 library("xgboost")
 library("Matrix")
@@ -8,8 +18,6 @@ library("purrr")
 ## train and predict function 
 
 train_and_predict = function(params, at, ...){
-  
-  packrat::init(project="~/Projects/otto")
   
   require("dplyr")
   require("Matrix")
@@ -53,17 +61,19 @@ train_and_predict = function(params, at, ...){
 }
 
 param_grid = expand.grid(
-  eta=c(0.03, 0.1, 0.3, 1),
-  max.depth=1:12,
-  subsample=c(0.5, 0.75, 1)
+  eta=eta,
+  max.depth=max.depth,
+  subsample=subsample
 )
 
 params =  zip(param_grid)
 
-more.args = list(at = 2^(4:12),
+
+more.args = list(at = at,
                  nrounds = max(at),
-                 objective = "multi:softprob",
-                 metrics=list("mlogloss")
+                 objective = objective,
+                 metrics=metrics,
+                 num_class=9
 )
 
 reg = makeRegistry("xgb_scan",
